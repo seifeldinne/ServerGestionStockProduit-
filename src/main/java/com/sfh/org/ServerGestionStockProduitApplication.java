4ce84aa32c75e1eabebb4da41b04c7f43600d6b8
unknown <seif.hanechi@gmail.com>
2018-11-15 22:31:29 +0100

    first_commit

---
+++ src/main/java/com/sfh/org/ServerGestionStockProduitApplication.java
@@ -0,0 +1,12 @@
+package com.sfh.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockProduitApplication.class, args);
+	}
+}
